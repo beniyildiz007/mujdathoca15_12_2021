dc50ae943d4cdd82f00a0b902502e5e3f7051404
nihat <nihat@DESKTOP-HKL3D7O>
2021-12-15 12:03:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mujdathoca15_12_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
